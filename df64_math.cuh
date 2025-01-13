a671ea8544b7054d8b024c6ebe4be59eb29031e7
dipto4 <dipto412@gmail.com>
2025-01-12 19:34:08 -0500

    added math functions

--- df64_math.cuh
+++ df64_math.cuh
@@ -1,8 +1,11 @@
 #pragma once
 #include "df64.cuh"
 
+#define ONE 1.0
+#define ZERO 0.0
 
-__host__ __device__ df64 sqrt(df64 a) {
+
+__host__ __device__ df64 sqrt(const df64 &a) {
     float xn = rsqrtf(a.val.x);
     float yn = a.val.x * xn;
     df64 ynsqr = yn;
@@ -15,23 +18,109 @@ __host__ __device__ df64 sqrt(df64 a) {
     return sum;
 }
 
-__host__ __device__ inline df64 log(const df64& a) {
 
+
+__host__ __device__ inline df64 exp(const df64& a) {
+    const float thresh = 1.0e-20 * exp(a->get_x());
+    df64 t, p , f, s, x;
+    float m;
+
+    s = a + df64(1.0f);
+    p = a * a;
+    m = 2.0f;
+    f = df64(2.0f);
+    t = p / df64(2.0f);
+
+    while(abs(t->get_x()) > thresh) {
+        s = s + t;
+        p = p * a;
+        m += 1.0f;
+        f = f * df64(m);
+        t = p / f;
+    }
+
+    return s + t;
+}
+
+
+__host__ __device__ inline df64 log(const df64& a) {
+    df64 xi;
+
+    if(!(a == 1.0f)) {
+        if(a.get_x() <= 0.0) {
+            xi  = make_float2(NAN, NAN);
+        } else {
+            xi.x = logf(a.get_x());
+            xi  = (xi + (exp(-xi) * a)) + df64(-1.0);  
+        }
+    }
+    return xi;
 }
  
 __host__ __device__ inline df64 sin(const df64& a) {
+    const float thresh = 1.0e-20 * abs(a->get(x));
+    df64 t, p , f , s, x;
+    float m;
 
+    df64 sin_a;
+
+    if(a->get_x() == 0.0f) {
+        sin_a =  df64(make_float2(ZERO,ZERO));
+    } else {
+        x = - (a * a);
+        s = a;
+        p = a;
+        m = ONE;
+        f = df64(make_float2(ONE,ZERO));
+
+        while(true) {
+            p = p * x;
+            m += 2.0f;
+            f = f * ds64(m*(m-1));
+            t = p / f;
+            s = s + t;
+            if(abs(t->get_x()) < thresh) {
+                break;
+            }
+        }
+        sin_a = s;
+    }
+
+    return sin_a;
 }
 
 __host__ __device__ inline df64 cos(const df64& a) {
+    const float thresh = 1.0e-20 * abs(a->get(x));
+    df64 t, p , f , s, x;
+    float m;
+
+    df64 cos_a;
+
+    if(a->get_x() == 0.0f) {
+        cos_a =  df64(make_float2(ONE,ZERO));
+    } else {
+        x = - (a * a);
+        s = a;
+        p = a;
+        m = ONE;
+        f = df64(make_float2(ONE,ZERO));
+
+        while(true) {
+            p = p * x;
+            m += 2.0f;
+            f = f * ds64(m*(m-1));
+            t = p / f;
+            s = s + t;
+            if(abs(t->get_x()) < thresh) {
+                break;
+            }
+        }
+        cos_a = sqrt(df64(ONE) + - (s * s));
+    }
+
+    return cos_a;
 
 }
-
-__host__ __device__ inline df64 exp(const df64& a) {
-
-}
-
-
 
 
 
